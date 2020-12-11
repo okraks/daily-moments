bfb6e6ccaa78d71cc64f766ea0d5c9861cb59676
Okraku Isaac Antwi <antwisaac7@gmail.com>
2020-12-11 15:56:14 +0000

    Project Set Up

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,25 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>My App</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        Add some content here…
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default HomePage;
